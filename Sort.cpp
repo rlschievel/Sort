f0a28864a9ed71f7db4a9b8fbc47fbc8c60ea496
rlschievel <rlschievel@gmail.com>
2016-05-23 22:45:28 -0500

    Add a search prompt function to print to console

--- Sort.cpp
+++ Sort.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main(int argc, char** argv){
-
+  printPrompt();
+  
   return 0;
+}
+
+void printPrompt(){
+  cout<<"Select a sort algorithm to run..."<<endl;
+  cout<<"  1: Bubble Sort"<<endl;
+  cout<<"  2: Selection Sort"<<endl;
+  cout<<"  3: Merge Sort"<<endl;
+  cout<<"  4: Quick Sort"<<endl;
+
+  return;
 }
