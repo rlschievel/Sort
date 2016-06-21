61602c49327391c2cd17927db58877a5ccedec18
rlschievel <rlschievel@gmail.com>
2016-06-21 00:54:01 -0500

    Implemented BubbleSort sort function

--- src/BubbleSort.cpp
+++ src/BubbleSort.cpp
@@ -2,6 +2,20 @@
 
 void BubbleSort::sort(std::vector<int> &values){
   //Add code for Bubble Sort algorithm here
+
+  bool swapped = true;
+  std::vector<int>::iterator it = values.begin();
+
+  while(swapped){
+    swapped = false;
+
+    for(it = values.begin(); it != values.end(); ++it){
+      if(*it > *(it + 1)){
+	Sort::swap(values, it + 1, it);
+	swapped = true;
+      }
+    }
+  }
   
   return;
 }
